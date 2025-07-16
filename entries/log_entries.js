341ce5cb93e518ff15d5ddc45280816b055c22c0
Pangbot <idonthaveaclueatall@hotmail.com>
2025-07-16 23:06:17 +0100

    Basic layout, some structure in game.js file.

---
+++ entries/log_entries.js
@@ -0,0 +1,10 @@
+export const log = [
+    {
+        id: "log_1",
+        conditions: 
+        {
+            day: 1
+        },
+        text: `A new visitor is the buzz of the village.`
+    }
+]
